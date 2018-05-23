d04c8e8d393ef388f8e403933e744d92724b35d5
Andrew Law <andrewlaw8@berkeley.edu>
2018-05-22 19:56:00 -0700

    asdf

---
+++ index.js
@@ -0,0 +1,40 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var users = {};
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/andrewlaw/personal/chat-simple/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('user connected');
+	io.emit('chat message', 'A user has connected.');
+
+	/*
+	io.emit('join', socket);
+
+	socket.on('join', function(name){
+		users[socket.id] = name;
+	});
+	*/
+
+	socket.on('chat message', function(msgObject) {
+		if (msgObject.name == "") {
+            users[socket.id] = "Anonymous";
+		} else {
+			users[socket.id] = msgObject.name;
+		}
+		io.emit('chat message', users[socket.id] + " : " + msgObject.msg);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+		io.emit('chat message', 'A user has disconnected.');
+	});
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
